105d3937799d681fd3320091805c1755127aece5
HarryDYC0506 <132695666+HarryDYC0506@users.noreply.github.com>
2023-06-02 10:54:10 -0700

    Change assets name

--- game.js
+++ game.js
@@ -4,8 +4,8 @@ class Coregameplay extends Phaser.Scene {
     }
     preload(){
         this.load.path = './assets';
-        this.load.image('roly poly', 'roly poly.png');
-        this.load.image('snails', 'snails.png');
+        this.load.image('roly poly', 'roly_poly.png');
+        this.load.image('snails', 'snail.png');
 
     }
 
